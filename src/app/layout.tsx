bd828f8fb0c69105de0ddba59c3f4df0fc4c90b3
sufjan bajric <sufjan.ba@gmail.com>
2025-09-11 11:03:31 +0000

    promijeni naslov u LinguAI-BKS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'LinguAIbks',
+  title: 'LinguAI-BKS',
   description: 'Učenje bosanskog, hrvatskog i srpskog jezika.',
 };
 
