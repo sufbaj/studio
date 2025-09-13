a853c5336eea6198a9a1c8410b9df2b15938ed61
sufjan bajric <sufjan.ba@gmail.com>
2025-09-13 07:04:12 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -25,10 +25,11 @@ const nextConfig: NextConfig = {
     ],
   },
   experimental: {
-    allowedDevOrigins: [
-      '*.cluster-jbb3mjctu5cbgsi6hwq6u4btwe.cloudworkstations.dev',
-    ],
+    
   },
+  allowedDevOrigins: [
+    '*.cluster-jbb3mjctu5cbgsi6hwq6u4btwe.cloudworkstations.dev',
+  ],
 };
 
 export default nextConfig;
