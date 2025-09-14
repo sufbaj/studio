4256e8c14d76e3d6e35065b498159de54a66a6e1
sufjan bajric <sufjan.ba@gmail.com>
2025-09-14 10:47:39 +0000

    pregledaj app i popravi sve sto nije uredu jer sad ne radi kako treba

---
+++ src/instrumentation.ts
@@ -0,0 +1,5 @@
+/**
+ * @fileoverview This file is used to register Genkit flows when the server starts.
+ * It is automatically loaded by Next.js due to the `instrumentationHook` experimental feature.
+ */
+import '@/ai/dev';
